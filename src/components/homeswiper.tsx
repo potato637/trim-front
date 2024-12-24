5463cb5f64a6e9713a13a8824088827ffa6c0d20
potatodev-777 <potatodev777@gmail.com>
2024-12-24 16:12:54 +0900

    progress bar

--- src/components/homeswiper.tsx
+++ src/components/homeswiper.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Grid } from "swiper/modules";
+import { Navigation, Grid, Pagination } from "swiper/modules";
 
 import "swiper/css";
+import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/grid";
 import { HomeDataStateI } from "../recoil/data";
@@ -32,15 +33,35 @@ const StyledSwiper = styled(Swiper)`
       font-size: 0.7rem;
     }
   }
+
+  .swiper-pagination-progressbar {
+    left: 50%;
+    transform: translateX(-50%); /* Adjust for the left offset */
+    height: 2px; /* Thickness of the progress bar */
+    background: #e0e0e0; /* Background for the progress bar (inactive part) */
+    border-radius: 1px; /* Smooth edges for the bar */
+    overflow: hidden; /* Ensure progress is contained within rounded corners */
+    width: 100%;
+  }
+
+  .swiper-pagination-progressbar-fill {
+    background: linear-gradient(90deg, #6a5acd, #855ff3); /* Gradient fill */
+    border-radius: 1px; /* Smooth edges for the fill */
+    height: 100%; /* Ensure the fill covers the height */
+    transition: transform 0.3s ease; /* Smooth transition for progress */
+  }
 `;
 
 export default function HomeSwiper({ data }: { data: HomeDataStateI[] }) {
   return (
     <StyledSwiper
-      modules={[Navigation, Grid]}
+      modules={[Navigation, Grid, Pagination]}
       slidesPerView={3}
       slidesPerGroup={3}
       navigation
+      pagination={{
+        type: "progressbar",
+      }}
       grid={{
         fill: "row",
         rows: 2,
