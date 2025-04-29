84a8ec74e8965fc7860a027da13fe5c418bdb110
potatodev-777 <potatodev777@gmail.com>
2025-04-29 23:21:49 +0900

    badges page 레이아웃 끝

--- src/pages/badges.tsx
+++ src/pages/badges.tsx
@@ -7,7 +7,6 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: yellow;
 `;
 const ControllerContainer = styled.div`
   position: absolute;
@@ -17,6 +16,31 @@ const ControllerContainer = styled.div`
 const EditBadges = styled.div`
   margin-top: 60px;
   width: 70%;
+  display: flex;
+  gap: 20px;
+`;
+const SelectedBadges = styled.div`
+  flex: 1;
+  padding: 15px;
+  background-color: var(--color-light-purple);
+  border: 1px solid var(--color-light-purple-border);
+  border-radius: 6px;
+  box-shadow: 0px 4px 14px rgba(93, 85, 108, 0.15);
+  & > span {
+    color: var(--color-purple-hover);
+    font-size: var(--font-size-small);
+  }
+`;
+const AllBadges = styled.div`
+  flex: 3;
+  padding: 15px;
+  background-color: var(--color-white-gray);
+  border-radius: 6px;
+  box-shadow: 0px 4px 14px 0px rgba(97, 96, 96, 0.15);
+  & > span {
+    color: var(--color-purple-hover);
+    font-size: var(--font-size-small);
+  }
 `;
 
 export default function Badges() {
@@ -25,7 +49,14 @@ export default function Badges() {
       <ControllerContainer>
         <Profilecontroller />
       </ControllerContainer>
-      <EditBadges></EditBadges>
+      <EditBadges>
+        <SelectedBadges>
+          <span>내 대표 뱃지</span>
+        </SelectedBadges>
+        <AllBadges>
+          <span>획득한 뱃지</span>
+        </AllBadges>
+      </EditBadges>
     </Container>
   );
 }
